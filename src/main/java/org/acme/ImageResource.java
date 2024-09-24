7a27a469d15da0361eb55d3d96bda50da6d9de0e
Georgios Andrianakis <geoand@gmail.com>
2024-09-24 16:13:19 +0300

    Simplify code with the new Image capabilities of Quarkus LangChain4j

--- src/main/java/org/acme/ImageResource.java
+++ src/main/java/org/acme/ImageResource.java
@@ -1,31 +1,31 @@
 package org.acme;
 
-import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.ImageContent;
-import dev.langchain4j.data.message.TextContent;
-import dev.langchain4j.data.message.UserMessage;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.output.Response;
+import io.quarkiverse.langchain4j.ImageUrl;
+import io.quarkiverse.langchain4j.RegisterAiService;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
 @Path("/image")
 public class ImageResource {
 
-    private final ChatLanguageModel chatLanguageModel;
+    private final CodeRecognizer codeRecognizer;
 
-    public ImageResource(ChatLanguageModel chatLanguageModel) {
-        this.chatLanguageModel = chatLanguageModel;
+    public ImageResource(CodeRecognizer codeRecognizer) {
+        this.codeRecognizer = codeRecognizer;
     }
 
     @GET
     @Path("describe")
     public String describe() {
-        UserMessage userMessage = UserMessage.from(
-                TextContent.from(
-                        "This is image was reported on a GitHub issue on the Quarkus repository. If this is a snippet of Java code, please respond with only the Java code. If it is not, describe what the image is showing"),
-                ImageContent.from("https://github.com/quarkusio/quarkus/assets/405347/8d57711b-4be5-437c-8fd4-90e542e65c3a"));
-        Response<AiMessage> response = chatLanguageModel.generate(userMessage);
-        return response.content().text();
+        return codeRecognizer.getCode("https://github.com/quarkusio/quarkus/assets/405347/8d57711b-4be5-437c-8fd4-90e542e65c3a");
+    }
+
+    @RegisterAiService(chatMemoryProviderSupplier = RegisterAiService.NoChatMemoryProviderSupplier.class)
+    @ApplicationScoped
+    private interface CodeRecognizer {
+
+        @dev.langchain4j.service.UserMessage("This is image was reported on a GitHub issue on the Quarkus repository. If this is a snippet of Java code, please respond with only the Java code. If it is not, describe what the image is showing")
+        String getCode(@ImageUrl String url);
     }
 }
